9ccd09dbbacbd0028e8f21f6dbe881942d343c2c
sosow0212 <sosow0212@naver.com>
2022-06-14 01:46:55 +0900

    test: AuthController 테스트코드 작성

--- src/test/java/yoon/community/controller/AuthControllerTest.java
+++ src/test/java/yoon/community/controller/AuthControllerTest.java
@@ -2,6 +2,7 @@ package yoon.community.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -10,69 +11,74 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import yoon.community.dto.sign.RegisterDto;
+import yoon.community.dto.sign.*;
 import yoon.community.service.AuthService;
 
+import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(MockitoExtension.class)
-class SignTest {
+class AuthControllerTest {
     @InjectMocks
     AuthController authController;
     @Mock
-    AuthService signService;
+    AuthService authService;
     MockMvc mockMvc;
-    ObjectMapper objectMapper = new ObjectMapper(); // 1
+    ObjectMapper objectMapper = new ObjectMapper();
 
     @BeforeEach
     void beforeEach() {
         mockMvc = MockMvcBuilders.standaloneSetup(authController).build();
     }
 
+    @DisplayName("회원가입 테스트")
     @Test
     void signUpTest() throws Exception {
         // given
-        RegisterDto req = new RegisterDto("test", "test", "test", "test");
+        RegisterDto req = new RegisterDto("test123", "test", "username", "nickname");
 
         // when, then
         mockMvc.perform(
-                        post("/api/auth")
+                        post("/auth/signup")
                                 .contentType(MediaType.APPLICATION_JSON)
-                                .content(objectMapper.writeValueAsString(req))) // 2
+                                .content(objectMapper.writeValueAsString(req)))
                 .andExpect(status().isCreated());
 
-        verify(signService).signup(req);
+        verify(authService).signup(req);
     }
 
-//    @Test
-//    void signInTest() throws Exception {
-//        // given
-//        LoginRequestDto req = new LoginRequestDto("test", "test");
-//        given(signService.signIn(req)).willReturn(new SignInResponse("access", "refresh"));
-//
-//        // when, then
-//        mockMvc.perform(
-//                        post("/api/sign-in")
-//                                .contentType(MediaType.APPLICATION_JSON)
-//                                .content(objectMapper.writeValueAsString(req)))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("$.result.data.accessToken").value("access")) // 3
-//                .andExpect(jsonPath("$.result.data.refreshToken").value("refresh"));
-//
-//        verify(signService).signIn(req);
-//    }
-
+    @DisplayName("로그인 테스트")
     @Test
-    void ignoreNullValueInJsonResponseTest() throws Exception { // 4
+    void signInTest() throws Exception {
         // given
-        RegisterDto req = new RegisterDto("test", "test", "test", "test");
+        LoginRequestDto req = new LoginRequestDto("test123", "test");
+        given(authService.signIn(req)).willReturn(new TokenResponseDto("access", "refresh"));
 
         // when, then
         mockMvc.perform(
-                        post("/api/auth")
+                        post("/auth/sign-in")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(req)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.result.data.accessToken").value("access"))
+                .andExpect(jsonPath("$.result.data.refreshToken").value("refresh"));
+
+        verify(authService).signIn(req);
+    }
+
+    @DisplayName("응답 JSON 테스트")
+    @Test
+    void ignoreNullValueInJsonResponseTest() throws Exception {
+        // 응답결과로 반환되는 JSON 문자열이 올바르게 제거되는지 검증
+        // given
+        RegisterDto req = new RegisterDto("test123", "test", "username", "nickname");
+
+        // when, then
+        mockMvc.perform(
+                        post("/auth/signup")
                                 .contentType(MediaType.APPLICATION_JSON)
                                 .content(objectMapper.writeValueAsString(req)))
                 .andExpect(status().isCreated())
