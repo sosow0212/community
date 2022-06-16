2e762a2957c691fb594d403afac965afb87cbead
sosow0212 <sosow0212@naver.com>
2022-06-16 20:33:21 +0900

    fix: Add Swagger info

--- src/main/java/yoon/community/config/SwaggerConfig.java
+++ src/main/java/yoon/community/config/SwaggerConfig.java
@@ -2,13 +2,23 @@ package yoon.community.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.util.Arrays;
+import java.util.List;
+
+@Import(BeanValidatorPluginsConfiguration.class)
 @Configuration
 // http://localhost:8080/swagger-ui/index.html
 public class SwaggerConfig {
@@ -16,20 +26,36 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.OAS_30)
-                .useDefaultResponseMessages(false)
+                .apiInfo(apiInfo())
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("yoon.community.controller"))
-                .paths(PathSelectors.any())
+                    .apis(RequestHandlerSelectors.basePackage("yoon.community.controller"))
+                    .paths(PathSelectors.any())
                 .build()
-                .apiInfo(apiInfo());
+                .securityContexts(Arrays.asList(securityContext()))
+                .securitySchemes(Arrays.asList(apiKey()));
     }
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Swagger Test")
-                .description("SwaggerConfig")
-                .version("3.0")
+                .title("Community")
+                .description("Community REST API Documentation")
+                .license("sosow0212@naver.com")
+                .licenseUrl("https://github.com/sosow0212/community")
+                .version("1.0")
                 .build();
     }
 
+    private static ApiKey apiKey() {
+        return new ApiKey("Authorization", "Bearer Token", "header");
+    }
+
+    private SecurityContext securityContext() {
+        return SecurityContext.builder().securityReferences(defaultAuth())
+                .operationSelector(oc -> oc.requestMappingPattern().startsWith("/api/")).build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "global access");
+        return List.of(new SecurityReference("Authorization", new AuthorizationScope[] {authorizationScope}));
+    }
 }
