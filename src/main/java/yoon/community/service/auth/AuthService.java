49890cd42cd4422a60fa2930ed99f3ba971eda97
sosow0212 <sosow0212@naver.com>
2022-07-23 11:27:44 +0900

    feat: Refactor - Saving RefreshToken from MySQL to Redis

--- src/main/java/yoon/community/service/auth/AuthService.java
+++ src/main/java/yoon/community/service/auth/AuthService.java
@@ -17,6 +17,7 @@ import yoon.community.exception.MemberNicknameAlreadyExistsException;
 import yoon.community.exception.MemberUsernameAlreadyExistsException;
 import yoon.community.repository.refreshToken.RefreshTokenRepository;
 import yoon.community.repository.user.UserRepository;
+import yoon.community.service.redis.RedisService;
 
 @Service
 @RequiredArgsConstructor
@@ -27,6 +28,7 @@ public class AuthService {
     private final PasswordEncoder passwordEncoder;
     private final TokenProvider tokenProvider;
     private final RefreshTokenRepository refreshTokenRepository;
+    private final RedisService redisService;
 
     @Transactional
     public void signup(SignUpRequestDto req) {
@@ -62,12 +64,13 @@ public class AuthService {
         TokenDto tokenDto = tokenProvider.generateTokenDto(authentication);
 
         // 4. RefreshToken 저장
-        RefreshToken refreshToken = RefreshToken.builder()
-                .key(authentication.getName())
-                .value(tokenDto.getRefreshToken())
-                .build();
+//        RefreshToken refreshToken = RefreshToken.builder()
+//                .key(authentication.getName())
+//                .value(tokenDto.getRefreshToken())
+//                .build();
+        redisService.setValues(authentication.getName(), tokenDto.getRefreshToken());
 
-        refreshTokenRepository.save(refreshToken);
+//        refreshTokenRepository.save(refreshToken);
         TokenResponseDto tokenResponseDto = new TokenResponseDto(tokenDto.getAccessToken(), tokenDto.getRefreshToken());
         // 5. 토큰 발급
         return tokenResponseDto;
@@ -76,31 +79,39 @@ public class AuthService {
 
     @Transactional
     public TokenResponseDto reissue(TokenRequestDto tokenRequestDto) {
-        // 1. Refresh Token 검증
-        if (!tokenProvider.validateToken(tokenRequestDto.getRefreshToken())) {
-            throw new RuntimeException("Refresh Token 이 유효하지 않습니다.");
-        }
+//        // 1. Refresh Token 검증
+//        if (!tokenProvider.validateToken(tokenRequestDto.getRefreshToken())) {
+//            throw new RuntimeException("Refresh Token 이 유효하지 않습니다.");
+//        }
+//
+//        // 2. Access Token 에서 Member ID 가져오기
+//        Authentication authentication = tokenProvider.getAuthentication(tokenRequestDto.getAccessToken());
+//
+//        // 3. 저장소에서 Member ID 를 기반으로 Refresh Token 값 가져옴
+//        RefreshToken refreshToken = refreshTokenRepository.findByKey(authentication.getName())
+//                .orElseThrow(() -> new RuntimeException("로그아웃 된 사용자입니다."));
+//
+//        // 4. Refresh Token 일치하는지 검사
+//        if (!refreshToken.getValue().equals(tokenRequestDto.getRefreshToken())) {
+//            throw new RuntimeException("토큰의 유저 정보가 일치하지 않습니다.");
+//        }
+//
+//        // 5. 새로운 토큰 생성
+//        TokenDto tokenDto = tokenProvider.generateTokenDto(authentication);
+//
+//        // 6. 저장소 정보 업데이트
+//        RefreshToken newRefreshToken = refreshToken.updateValue(tokenDto.getRefreshToken());
+//        refreshTokenRepository.save(newRefreshToken);
+//
+//        // 토큰 발급
+//        TokenResponseDto tokenResponseDto = new TokenResponseDto(tokenDto.getAccessToken(), tokenDto.getRefreshToken());
+//        return tokenResponseDto;
 
-        // 2. Access Token 에서 Member ID 가져오기
         Authentication authentication = tokenProvider.getAuthentication(tokenRequestDto.getAccessToken());
+        redisService.checkRefreshToken(authentication.getName(), tokenRequestDto.getRefreshToken());
 
-        // 3. 저장소에서 Member ID 를 기반으로 Refresh Token 값 가져옴
-        RefreshToken refreshToken = refreshTokenRepository.findByKey(authentication.getName())
-                .orElseThrow(() -> new RuntimeException("로그아웃 된 사용자입니다."));
-
-        // 4. Refresh Token 일치하는지 검사
-        if (!refreshToken.getValue().equals(tokenRequestDto.getRefreshToken())) {
-            throw new RuntimeException("토큰의 유저 정보가 일치하지 않습니다.");
-        }
-
-        // 5. 새로운 토큰 생성
+        // 예외 처리 통과후 토큰 재생성
         TokenDto tokenDto = tokenProvider.generateTokenDto(authentication);
-
-        // 6. 저장소 정보 업데이트
-        RefreshToken newRefreshToken = refreshToken.updateValue(tokenDto.getRefreshToken());
-        refreshTokenRepository.save(newRefreshToken);
-
-        // 토큰 발급
         TokenResponseDto tokenResponseDto = new TokenResponseDto(tokenDto.getAccessToken(), tokenDto.getRefreshToken());
         return tokenResponseDto;
     }
