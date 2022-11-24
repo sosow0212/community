f082190671ff342283c3c5402b399e311cb086d6
sosow0212 <sosow0212@naver.com>
2022-11-24 13:54:56 +0900

    feat : (EntityDate) 모든 테이블의 공통 필드인 생성 및 수정일 필드가 있는 EntityDate 공통 테이블 생성

---
+++ src/main/java/yoon/community/entity/common/EntityDate.java
@@ -0,0 +1,23 @@
+package yoon.community.entity.common;
+
+import java.time.LocalDateTime;
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+@Getter
+public abstract class EntityDate {
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime modifiedAt;
+}
