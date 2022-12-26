2b3f241f6f12129a2968468c88200a32db023c2a
sosow0212 <sosow0212@naver.com>
2022-12-26 16:05:03 +0900

    test : (Category Test) Category 도메인 테스트 진행

---
+++ src/test/java/yoon/community/domain/CategoryTest.java
@@ -0,0 +1,20 @@
+package yoon.community.domain;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import yoon.community.entity.category.Category;
+
+public class CategoryTest {
+    @Test
+    @DisplayName("카테고리 생성자 테스트")
+    public void categoryConstructorTest() {
+        // given, when
+        String name = "카테고리";
+        Category category = new Category(name, null);
+
+        // then
+        assertThat(category.getName()).isEqualTo(name);
+    }
+}
